3a1d7c9cfc06cb6d7b3756200df8c8333b7989bc
umitishere <umitsancakli98@gmail.com>
2021-08-14 11:51:07 +0300

    Initialize the website

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Video Streaming</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+<body>
+
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+            hey
+        </div>
+
+        <div id="sideNavContainer">
+            hey
+        </div>
+
+        <div id="mainSectionContainer">
+            hey
+        </div>
+
+    </div>
+
+</body>
+
+</html>
