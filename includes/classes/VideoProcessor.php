9e4a83b9a2256cbcf9466169add59baa094de919
umitishere <umitsancakli98@gmail.com>
2021-08-17 13:12:14 +0300

    Create the insert video function

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -25,7 +25,14 @@ class VideoProcessor {
         }
 
         if (move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
-            echo "File moved successfully";
+
+            $finalFilePath = $targetDir . uniqid() . ".mp4";
+
+            if (!$this->insertVideoData($videoUploadData, $finalFilePath)) {
+                echo "Insert query failed.";
+                return false;
+            }
+
         }
     }
 
@@ -61,6 +68,25 @@ class VideoProcessor {
 
     private function hasError($data) {
         return $data["error"] != 0;
+    }
+
+    private function insertVideoData($uploadData, $filePath) {
+
+        $query = $this->con->prepare("INSERT INTO videos
+            (title, uploadedBy, description, privacy, category, filePath)
+            VALUES
+            (:title, :uploadedBy, :description, :privacy, :category, :filePath)
+        ");
+
+        $query->bindParam(":title", $uploadData->title);
+        $query->bindParam(":uploadedBy", $uploadData->uploadedBy);
+        $query->bindParam(":description", $uploadData->description);
+        $query->bindParam(":privacy", $uploadData->privacy);
+        $query->bindParam(":category", $uploadData->category);
+        $query->bindParam(":filePath", $filePath);
+
+        return $query->execute();
+
     }
 
 }
